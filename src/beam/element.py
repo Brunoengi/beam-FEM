181f9d7d838172046ffa421cdfff77c814a98249
Bruno Santos <bruno.if@hotmail.com>
2025-12-17 02:25:49 -0300

    created element without Degree of Freedom (DOF)

--- src/beam/element.py
+++ src/beam/element.py
@@ -1,4 +1,5 @@
 import sympy as sp
+import math
 
 class Element:
     """
@@ -24,6 +25,8 @@ class Element:
     end : float
         Final coordinate of the element along the local x-axis  
         Unit: cm
+    theta: float
+        Unit: Graus
 
     Notes
     -----
@@ -34,22 +37,26 @@ class Element:
         Stress / modulus → kN/cm²
     """
 
-    def __init__(self):
+    def __init__(self, I_, E_, A_, start, end, theta_=0.0):
         
         sp.init_printing()
         
-        # self.I = I
-        # self.E = E
-        # self.A = A
-        # self.start = start
-        # self.end = end
-        # self.L = end - start
+        self.I = I_
+        self.E = E_
+        self.A = A_
+        self.start = start
+        self.end = end
+        self.L = end - start
         
+        ##Defined in terms of symbolic variables
         I, E, A, L, theta, c, s = self.create_symbols()
-        ke_ = self.ke_(E, A, I, L)
-        T = self.T(c,s)
-        self.Ke = self.Ke(T, ke_)
-        print(self.Ke)
+        symbolic_ke_local = self.symbolic_ke_(E, A, I, L)
+        T = self.T(c, s)
+        symbolic_ke_global = self.symbolic_ke(T, symbolic_ke_local)
+        
+        
+        ke_global = self.calculate_ke(symbolic_ke_global, I_, A_, E_, self.L, theta_, I, E, A, L, theta)
+        print(ke_global)
         
     def create_symbols(self):
         I, E, L, A, theta = sp.symbols(['I', 'E', 'L', 'A', 'theta'])
@@ -57,7 +64,7 @@ class Element:
         s = sp.sin(theta)
         return I, E, L, A, theta, c, s
   
-    def ke_(self, E, A, I, L):
+    def symbolic_ke_(self, E, A, I, L):
         """
         Computes the local stiffness matrix of a beam element
         based on Euler–Bernoulli beam theory. The element has 6 degrees of freedom.
@@ -88,7 +95,7 @@ class Element:
         ])
         return ke_
     
-    def T(self, c,s):
+    def T(self, c, s):
         T = sp.Matrix([
             [c, -s, 0, 0, 0, 0],
             [s, c, 0, 0, 0, 0],
@@ -96,11 +103,20 @@ class Element:
             [0, 0, 0, c, -s, 0],
             [0, 0, 0, s, c, 0],
             [0, 0, 0, 0, 0, 1]  
-        ])
+        ]).T
         return T
     
-    def Ke(self, T, ke_):
+    def symbolic_ke(self, T, ke_):
         return T.T * ke_ * T
     
+    def calculate_ke(self, ke, I_, A_, E_, L_, theta_, I, E, A, L, theta):
+        """
+        Replace symbolic values ​​with numerical values
+        """
+        theta_ = theta_ *math.pi /180.
+        k_element = ke.subs({I: I_, A: A_, E: E_, L: L_, theta: theta_})
+        return k_element
+    
+    
+el = Element(300, 70e3, 40., 0, 200, 30)
 
-ele = Element()
